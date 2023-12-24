ef49e28d9533dc29c52632edc09890d096974b98
ShaileshRimal <sailrim215@gmail.com>
2023-12-24 12:13:16 +0545

    first pythonclass

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,107 @@
-printf("hello world")
+# age=20
+# name="shailesh"
+# print(name)
+# print(age)
+# name=input("What is the person's name ? ")
+# age=(input("what is his age ? "))
+# level= input("who is he ? ")
+# print("\n---------------------------------------")
+# print(name) 
+# print("Age = "+ age)
+# print(level)
+
+
+
+# name=input("who is your favourite superhero ? ")
+# print(name)
+
+# first= int(input("enter 1st number "))
+# second=int(input("enter 2nd number "))
+# ans=first+second
+# # print(ans)
+# name="shailesh rimal"
+# print(name.upper())
+# print(name.find('l'))
+# print(name.replace("shail", "pail"))
+# print("rim" in name)
+# print(52)
+# i=5
+# i+=2
+# print(i)
+# print(2!=4)
+
+
+# # print(3>2 and 4>3 )
+# age =int(input("enter your age: "))
+# if(age>=18):
+#     print("you are an adult")
+#     print("you can vote")
+
+# elif age<18 and age>3:
+#     print("you are in shcool")
+
+# else:
+#     print("you are a child")
+
+
+#code to make calculator
+
+# while True:
+#     first = int(input("Enter first number : "))
+#     Operator=(input("Enter Operator('+','-','*','/','%'): "))
+#     Second =int(input("Enter Second number :" ))
+
+#     if(Operator== "+"):print(first+Second)
+
+#     elif(Operator== "-"):print(first-Second)
+
+#     elif(Operator== "*"): print(first*Second)
+
+#     elif(Operator== "/"):print(first/Second)
+
+#     elif(Operator== "%"):print(first%Second)
+#     else:print("INVALID OPERATOR ")
+
+#     want=(input("want to continue ?(Y/N)"))
+
+#     if(want=="n" or want=='N'):break
+#     if(want=="y" or want=='Y'):continue
+#     if(want!="n" or want!='N'):break
+    
+
+
+
+# numbers =range(5)
+# print(numbers)
+# i =1
+# while i <=5:
+#     print(i*"helo")
+#     i=i+1
+
+# for numbers in range(100):
+#     print(numbers%10)
+
+# marks=[1,2,5,6,7,85,4,32]
+# # print(marks[0:5])
+# for numbers in marks:
+#     print(numbers)
+
+# marks.insert(0,56)
+# print(4 in marks)
+# marks.clear()
+# # print(marks)
+# students= ["ram", "shyam","kishan","radha"]
+# for student in students:
+#     if student=="shyam":
+#         continue
+# #     print(student)
+# numbers=(1,2,4,5,4,8,9)
+# print(numbers.index(4))
+# name={"shyam":12, "ram":43, "hari":33 }
+# print(name)
+# print(name.clear())
+from math import pi
+print(pi)
+# from math import * 
+
+# print(pow(64))
